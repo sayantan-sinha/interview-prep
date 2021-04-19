63e43e37e7900264af33e69c1b06fbc2829f3c68
Sayantan Sinha <sayantansinha85@gmail.com>
2021-04-20 00:23:50 +0530

    canSum memoised

--- dynamic_prog/canSum_memo.cpp
+++ dynamic_prog/canSum_memo.cpp
@@ -2,15 +2,19 @@
 
 using namespace std;
 
-bool canSum(int target, vector<int> &arr) {
+bool canSumMemo(int target, vector<int> &arr, unordered_map<int, bool> &memo) {
 	bool res;
-		
+	
+	if(memo.count(target)!=0)
+		return memo[target];
+	
 	if(target==0)
 		return true;
 	
 	for(int num : arr) {
 		if(target-num >= 0) {
-			res = canSum(target-num, arr);
+			res = canSumMemo(target-num, arr, memo);
+			memo[target] = res;
 			if(res) 
 				return true;
 		}
@@ -21,20 +25,20 @@ bool canSum(int target, vector<int> &arr) {
 
 int main() {
 	vector<int> arr;
-	int target, num;
+	int num, target;
 	bool res;
-	
-	//Taking array as input
 	string line;
-	cout << "Enter array elements separated by space: ";
+	
+	// Taking array input
+	cout << "Enter array nums: ";
 	getline(cin, line);
 	istringstream stream(line);
 	while(stream >> num) {
 		arr.push_back(num);
 	}
-
-	// Taking target as input
-	cout << "Enter Target: ";
+	
+	// Taking target output
+	cout << "Enter target sum: ";
 	cin >> target;
 	
 	// Verification
@@ -48,12 +52,13 @@ int main() {
 	*/
 	
 	// Call function
-	res = canSum(target, arr);
+	unordered_map<int, bool> memo;
+	res = canSumMemo(target, arr, memo);
 	
 	if(res==true)
 		cout << "Possible" << endl;
 	else 
 		cout << "Not Possible" << endl;
-		
+	
 	return 0;
 }
