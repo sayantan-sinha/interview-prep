19b0ccccef1b3a1d2389cce718c3ffa12f73e279
Sayantan Sinha <sayantansinha85@gmail.com>
2021-04-23 00:21:21 +0530

    best sum brute force

--- dynamic_prog/bestSum.cpp
+++ dynamic_prog/bestSum.cpp
@@ -2,39 +2,39 @@
 
 using namespace std;
 
-vector<int> howSumMemo(int sum, vector<int> &arr, unordered_map<int, vector<int>> &memo) {
-	vector<int> res, v = {-1};
-		
-	if(memo.count(sum) != 0) return memo[sum];
+vector<int> bestSum(int target, vector<int> arr) {
+
+	vector<int> temp, res = {-1}, v = {-1};
+	int min = numeric_limits<int>::max();
+
+	if(target == 0) return vector<int> ();
 	
-	if(sum == 0) return vector<int>();
+	if(target < 0) return v;
 	
-	if(sum < 0) return v;
-	
-	for(int num : arr) {
-		res = howSumMemo(sum-num, arr, memo);
-		if(res != v) {
-			res.push_back(num);
-			memo[sum] = res;
-			return res;
-		} else {
-			memo[sum] = res;
+	for(auto num: arr) {
+		temp = bestSum(target-num, arr);
+		if(temp != v) {
+			temp.push_back(num);
+			if(min > temp.size()) {
+				min = temp.size();
+				res = temp;
+			}
 		}
 	}
-	
-	return v;
-}
 
+	if(res == v) return v;
+
+	return res;
+}
 
 // m = target sum
 // n = length of array
 
-// time: O(n*m^2)
-// space: O(m^2)
+// time: O(n^m * m) 
+// space: O(m * m)-> m stack frames each frame storing one copy of res which can be of m length at max
 
 int main() {
 	vector<int> arr, res;
-	unordered_map<int, vector<int>> memo;
 	int num, sum;
 	string line;
 	
@@ -44,14 +44,14 @@ int main() {
 	while(stream >> num) {
 		arr.push_back(num);
 	}
-	
+
 	cout << "Enter Target: ";
 	cin >> sum;
 
-	res = howSumMemo(sum, arr, memo);
-	
+	res = bestSum(sum, arr);
+
 	vector<int> v = {-1};
-	
+
 	if(res == v) {
 		cout << "Not Possible" << endl;
 	}	else {
@@ -60,7 +60,7 @@ int main() {
 			cout << *it << ",";
 		} while(++it != res.end());
 	}
-	
+
 	cout << endl;
 
 	return 0;
