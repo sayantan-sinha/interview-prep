6e9a37c7bcc38ba896fcd4e549fdf137804aa5b4
Sayantan Sinha <sayantansinha85@gmail.com>
2021-05-05 23:31:14 +0530

    canSum tabulation

---
+++ dynamic_prog/can_sum_tab.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+	int target, num;
+	string line;
+	vector<int> arr;
+	cout << "Enter target sum: ";
+	cin >> target;
+	//to clear buffer of the next line
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	
+	cout << "Enter array values separated by space: ";
+	getline(cin, line);
+	istringstream stream(line);
+	while(stream >> num) {
+		arr.push_back(num);
+	}
+	
+	vector<bool>sum(target+1, false);
+	sum[0] = true;
+	for(int i=0; i<=target; i++) {
+		if(sum[i] == true)
+			for(int j=0; j<=arr.size(); j++) {
+				if(i+arr[j] <= target)
+					sum[i+arr[j]] = true;
+			}
+	}
+		
+	cout << endl << sum[target] << endl;
+	return 0;
+}
